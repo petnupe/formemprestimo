92a0722e610e9e2bc62bf49dbb7b3b8e9a9b0cac
Peterson Pedroso <pemadata@gmail.com>
2024-06-06 09:43:37 -0300

    .

--- src/envia.php
+++ src/envia.php
@@ -1,18 +1,19 @@
 <?php
-
 include_once("./sendmail.php");
 
+
+$baseDir = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'uploads') . DIRECTORY_SEPARATOR;
 $mensagem = null;
 foreach ($_REQUEST as $key => $value)
     $mensagem .= ucfirst(str_replace("_", " ", $key)) . ": " . $value . "<br />";
 
-move_uploaded_file($_FILES['identidade']['tmp_name'], '../uploads/' . basename($_FILES['identidade']['name']));
-move_uploaded_file($_FILES['comprovante_endereco']['tmp_name'], '../uploads/' . basename($_FILES['comprovante_endereco']['name']));
-move_uploaded_file($_FILES['outro_documento']['tmp_name'], '../uploads/' . basename($_FILES['outro_documento']['name']));
+move_uploaded_file($_FILES['identidade']['tmp_name'], $baseDir . basename($_FILES['identidade']['name'])) or die('Sem permissao');
+move_uploaded_file($_FILES['comprovante_endereco']['tmp_name'], $baseDir . basename($_FILES['comprovante_endereco']['name'])) or die('Sem permissao');
+move_uploaded_file($_FILES['outro_documento']['tmp_name'], $baseDir . basename($_FILES['outro_documento']['name'])) or die('Sem permissao');
 
-$rg = '../uploads/' . basename($_FILES['identidade']['name']);
-$compResidencia = '../uploads/' . basename($_FILES['comprovante_endereco']['name']);
-$outro_documento = '../uploads/' . basename($_FILES['outro_documento']['name']);
+$rg = $baseDir . basename($_FILES['identidade']['name']);
+$compResidencia = $baseDir . basename($_FILES['comprovante_endereco']['name']);
+$outro_documento = $baseDir . basename($_FILES['outro_documento']['name']);
 
 //$arrayDestinos = array('peterson@tecbiz.com.br', 'credenciamento@tecbiz.com.br', 'oseias@tecbiz.com.br');
 $arrayDestinos = array('peterson@tecbiz.com.br');
