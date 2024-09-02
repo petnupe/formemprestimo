a02796afb0b0faba577ae11b0f35466bfc93f9d0
petnupe <pemadata@gmail.com>
2024-09-02 08:55:53 -0300

    Removendo https

--- form.php
+++ form.php
@@ -3,13 +3,11 @@
 if (!isset($_POST['hash']) || $_POST['hash'] != md5(date('Y-m-d')))
     die('Esta p&aacutegina n&atilde;o pode ser acessad diretamente');
 
-//$host = 'http://192.168.15.30:8080/tecbiz/tecbiz.php?a=5e7ae4&cartao=' . str_replace(" ", "", $_POST['cartao']);
-$host = 'https://www2.tecbiz.com.br/tecbiz/tecbiz.php?a=5e7ae4&cartao=' . str_replace(" ", "", $_POST['cartao']);
+$host = 'http://www2.tecbiz.com.br/tecbiz/tecbiz.php?a=5e7ae4&cartao=' . str_replace(" ", "", $_POST['cartao']);
 $json = utf8_decode(file_get_contents($host));
 $dadosAssociado = json_decode($json);
 
 if (json_last_error() > 0) {
-    //$host = 'https://www2.tecbiz.com.br/tecbiz/tecbiz.php?a=5e7ae4&cartao=' . str_replace(" ", "", $_POST['cartao']);
     $json = file_get_contents($host);
     $dadosAssociado = json_decode($json);
 }
