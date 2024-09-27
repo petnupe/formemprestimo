fa92345f1152c68163400077667682dcc83cc878
petnupe <pemadata@gmail.com>
2024-09-27 12:24:36 -0300

    gmail

--- src/sendmail.php
+++ src/sendmail.php
@@ -19,7 +19,7 @@ class sendMail
 
                 $config = $this->getConfigGmail();
 
-                $sendPulse = true;
+                //$sendPulse = true;
 
                 if ($sendPulse == true) {
                         $config = $this->getConfigSendPulse();
